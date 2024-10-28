6c7f954e9297c4ae3de8adac3706f1f6d7ccd8e1
nebilesamelsuleymna <nebilesmaelprog@gmail.com>
2024-10-28 10:10:33 -0700

    jwt

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express=require('express');
 const AppError =require('./utils/AppError')
+const globalErorhandler=require('./../controller/errorController')
 const app=express()
+
 
 
 app.get('/',(req,res)=>{
@@ -11,14 +13,6 @@ app.all('*',(req,res,next)=>{
     next(new AppError(`cant find ${req.originalurl} on th is server`,400))
 })
 
-app.use((err,req,res,next)=>{
-    err.statusCode =err.statusCode ||500 
-    err.status =err.status || 'error';
-    
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    })
-})
+app.use(globalErorhandler)
 
 module.exports=app
